91a2c52371acb2786060f09eceac63ae6e242c38
MrDjoy <1023747194@qq.com>
2024-02-07 11:57:18 +0800

    Update test.php

--- data/test.php
+++ data/test.php
@@ -4,13 +4,13 @@ $config = array(
     'host' => "localhost",
     'port' => "3306",
     'user' => "root",
-    'passwd' => "1023747194",
+    'passwd' => "yourpassword",
     'charset' => "utf8",
     'dbname' => "shop"
 );
 $db = mysql::getInstance($config);
-// $passwd = md5("Djoy");
-// $sql = "insert coolshop_admin(username,password,email) values('Djoy','{$passwd}','1023747194@qq.com')";
+// $passwd = md5("username");
+// $sql = "insert coolshop_admin(username,password,email) values('Djoy','{$passwd}','your@qq.com')";
 // $db->exec($sql);
 $sql = "select *from coolshop_admin;";
 $result = $db->getRows($sql);
